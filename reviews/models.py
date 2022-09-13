2f9ed1b6f96f36f327a2d60a34070f5a47fdc199
az85 <aaron.zahl@gmail.com>
2022-09-13 10:36:00 +0100

    added rating field to user reviews model

--- reviews/models.py
+++ reviews/models.py
@@ -9,6 +9,7 @@ class Review(models.Model):
     created_at = models.DateTimeField(auto_now=True)
     # one to many realitionship
     # it's important that this is added to the many (the field should be on the many, in this case the review)
+    rating = models.PositiveIntegerField(default=None)
     album = models.ForeignKey(
         "albums.Album",  # this argument specifies first the app name and the name of the model class inside of models.py
         # this argument is specifying the name of the field the review data will be returned under when querying the books
